e8bf306aa35ebf9211727c7f0160665a7acb6590
LucasAdamyMartin <lucasadamy123@gmail.com>
2025-03-01 13:02:04 +0100

    x

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,16 @@ function App() {
   ]
   return (
     <div id="container">
-    <img id="image" src='/images/avatar-jessica.jpeg' alt="avatar"/>
-    <h2 id="name">Jessica Randall</h2>
-    <h3 id="city">London, United Kingdom</h3>
-    <h3 id="description">"Front-end developer and avid reader."</h3>
-    {NavigationItems.map((item) => (
-      <div id="link" key={item}>{item}</div>
-    ))}
-    
+      <img id="image" src='/images/avatar-jessica.jpeg' alt="avatar"/>
+      <h2 id="name">Jessica Randall</h2>
+      <h3 id="city">London, United Kingdom</h3>
+      <p id="description">"Front-end developer and avid reader."</p>
+      <div id="links">
+        {NavigationItems.map((item) => (
+            <p id="link" key={item}>{item}</p>
+        ))}
+      </div>
+
     </div>
   )
 }
