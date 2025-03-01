10f67aa96d297c821f3163650f90b33ec5bcb8d8
LucasAdamyMartin <lucasadamy123@gmail.com>
2025-03-01 09:17:22 +0100

    begin projet

---
+++ src/styles/colors.ts
@@ -0,0 +1,9 @@
+const colors = {
+    primary: "#333333",
+    secondary: "#c4f82a",
+    backgroundBody: "#141414",
+    background : "#1f1f1f",
+    text: "#333"
+  };
+  
+  export default colors;
